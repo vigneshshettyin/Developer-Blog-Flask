cc75ab6abfe6e4939ccadba6333a1ffe3122b0eb
Vignesh <contact@vigneshshetty.in>
2021-02-04 20:22:54 +0530

    Custom 404 Page

--- app.py
+++ app.py
@@ -278,5 +278,9 @@ def logout():
     flash('Logged out successfully!', 'success')
     return redirect(url_for('loginPage'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
